c3f843e2598119912a0200065f24a92305a5cd89
Dan Metzkr <danilometzker@gmail.com>
2020-07-11 10:35:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 	<title>LitePlayer</title>
 
-	<link rel="stylesheet" href="src/liteplayer.css" />
+	<link rel="stylesheet" href="src/liteplayer.css"/>
 </head>
 <body>
 
